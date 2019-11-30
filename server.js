c8af37dfa89bb6e25446798408fc5cf92aabcd18
Henry <henry88lay@gmail.com>
2019-11-30 20:16:56 +1100

    connected MongoDB to app

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => 
-res.json({
-    msg: 'Welcome to the ContactKeeper API...'
-}));
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) =>
+    res.json({
+        msg: 'Welcome to the ContactKeeper API...'
+    }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
